1f174dd54c74c0a9f43f78f909e7fa14ac80e6e2
Vansh <vanshsaxena2022@gmail.com>
2026-01-15 18:57:07 +0530

    Fix DB connection for Render

--- server.js
+++ server.js
@@ -24,10 +24,14 @@ app.get("/debug/env", (req, res) => {
 /* ===========================
    DATABASE
 =========================== */
+const { Pool } = require("pg");
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
+
+module.exports = pool;
 
 /* ===========================
    BOOT ROUTES
